87318ebd38aa304e190103393562e1803533784d
QSweet <QoobSweet@gmail.com>
2017-05-31 13:21:09 -0400

    0.13
    LoadScreen Implemented

--- resources/QLoadScreen/__resource.lua
+++ resources/QLoadScreen/__resource.lua
@@ -1,7 +1,10 @@
-server_scripts {
-
+files {
+    'index.html',
+    'wl_load.css',
+    'bankgothic.ttf',
+    'images/images1.jpg', 'images/image2.jpg', 'images/l_1.png', 'images/l_2.png'
 }
 
-client_scripts {
+loadscreen 'index.html'
 
-}
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
