46caf6086ff4b1bee38e4289b91e975b6edea651
QSweet <QoobSweet@gmail.com>
2017-06-16 13:08:58 -0400

    0.12
    test values for database_actions_lua removed. Update script now finished

--- resources/QCoreEssential/server/database_actions.lua
+++ resources/QCoreEssential/server/database_actions.lua
@@ -8,32 +8,30 @@ require "resources/QCoreEssential/lib/MySQL"
 
 --either Push specific information
 AddEventHandler("UpdateDB", function(dbname, target)
-
-  local target = {["id"]="identifier", ["x_coord"]=123, ["y_coord"]=456, ["z_coord"]=789}
-  local test = {["id"]="identifier", ["y_coord"]=456, ["z_coord"]=789}
-  local assign_1 = ""
-  local assign_2 = ""
-  local dbname = "testdb"
   --compare database fields and insert missing fields, assigned with correct variable types
-  if target.id == nil then
-    print("identifier not set")
+  if target == nil then
+    print("target in push not set or passed incorectly!")
   else
+    if target.id == nil then
+      print("identifier not set")
+    else
 
-    local counter = 0
-    for key, value in pairs(target) do
-      if value == nil then
-        print("ERROR: "..key.." has no value")
-      else
-        MySQL.Async.fetchAll("SELECT * FROM "..dbname, {}, function(test) 
-          if test[key] == nil then                                      --check against existing values of other accounts and create any missing fields
-            MySQL.Async.execute("INSERT INTO "..dbname.." (`"..key.."`) VALUES ('"..value.."')")
-          end
-        end)
-        counter = counter + 1 
-        if counter == 1 then                                              -- start building input data
-          assign_1 = "'"..key.."'='"..value.."'"
+      local counter = 0
+      for key, value in pairs(target) do
+        if value == nil then
+          print("ERROR: "..key.." has no value")
         else
-          assign_1 = assign_1..", '"..key.."'='"..value.."'" 
+          MySQL.Async.fetchAll("SELECT * FROM "..dbname, {}, function(test) 
+            if test[key] == nil then                                      --check against existing values of other accounts and create any missing fields
+              MySQL.Async.execute("INSERT INTO "..dbname.." (`"..key.."`) VALUES ('"..value.."')")
+            end
+          end)
+          counter = counter + 1 
+          if counter == 1 then                                              -- start building input data
+            assign_1 = "'"..key.."'='"..value.."'"
+          else
+            assign_1 = assign_1..", '"..key.."'='"..value.."'" 
+          end
         end
       end
     end
